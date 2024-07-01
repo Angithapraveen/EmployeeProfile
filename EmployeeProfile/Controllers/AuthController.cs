e8cf7b6b1af99f69a6b71102bcdb177142aa62fb
Abhin <Abhin@ANGITHA>
2024-07-01 11:09:03 +0530

    Add project files.

---
+++ EmployeeProfile/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeProfile.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeProfile.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly TokenService _tokenService;
+
+        public AuthController(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            // Validate user credentials (this is just a simple example)
+            if (login.Username == "test" && login.Password == "password")
+            {
+                var token = _tokenService.GenerateToken(login.Username);
+                return Ok(new { token });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
+
+public class LoginModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ EmployeeProfile/Models/Entity/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeProfile.Models.Entity
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
